e683392be008dd3856f79ee44f35cdba4114c5b1
test19 <angjeffrey7376@gmail.com>
2020-08-25 14:45:25 +0800

    v1.1

--- VCS.py
+++ VCS.py
@@ -1,1 +1,15 @@
-print('Testing')
+import requests
+url = 'http://172.18.58.238/nantes/'
+r = requests.get(url)
+print("Status code:")
+print("\t *", r.status_code)
+h = requests.head(url)
+print("Nantes")
+print("----get----")
+for x in h.headers:
+    print("\t ", x, ".", h.headers[x])
+print("----OK----")
+headers = { 'User-Agent' : 'Mobile'}
+url2 = 'http://172.18.58.238/headers.php'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
